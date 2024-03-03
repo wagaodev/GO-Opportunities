e3fd641d53716a5f6456405923aeccb4fbfcb550
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2024-03-03 19:37:09 -0300

    feat: create a formatted and non-formatted logs handler

---
+++ config/config.go
@@ -0,0 +1,13 @@
+package config
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Init() error {
+	return nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/wagaodev/Go-Opportunities/config"
 	"github.com/wagaodev/Go-Opportunities/router"
 )
 
 func main() {
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	router.Initialize()
 }
