67c7c8d167fbc9bf53a61c870034eeba25793044
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2024-03-03 03:16:18 -0300

    refactor: change the sub modules

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/wagaodev/Go-Opportunities/router"
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run()
+	router.Initialize()
 }
