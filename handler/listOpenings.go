3f1298aaf7137668f0f5d85ca0c4d0142f962152
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2024-03-04 04:22:08 -0300

    feat: create list and delete openings

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/wagaodev/Go-Opportunities/schemas"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	openings := []schemas.Opening{}
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSuccess(ctx, "list-openings", openings)
 }
